dc71dcb785de77aeabb349abba6be4413559869f
David Villegas <dville00@gmail.com>
2017-08-17 23:07:31 -0400

    Some cleanup

--- game/js/index.js
+++ game/js/index.js
@@ -5,12 +5,13 @@ const TERRAIN_SEGMENTS = 32; // Must be 2^n
 const TERRAIN_SCALE_FACTOR = 4;
 const TERRAIN_SIDE_VERTICES = TERRAIN_SEGMENTS + 1;
 
-//const TERRAIN_VERTICES = TERRAIN_SIDE_VERTICES * TERRAIN_SIDE_VERTICES;
 const TERRAIN_INITIAL_DAMPING = 200;
 const TERRAIN_DAMPING_FACTOR = 0.9;
 
 const AFRAME = window.AFRAME;
 const THREE = window.THREE;
+
+/**** Terrain generation ******/
 
 const rr = () => Math.random() * Math.random();
 const coordsToIndex = (x,y) => {
@@ -18,15 +19,21 @@ const coordsToIndex = (x,y) => {
     (TERRAIN_SIDE_VERTICES * ((y + TERRAIN_SIDE_VERTICES) % TERRAIN_SIDE_VERTICES));
 };
 
-
-function averagePoints(vertices, tl, tr, bl, br, damping) {
+/**
+ * Calculate the average of the z-values of four vertices, then add the
+ * elevation multiplied by a random number
+ */
+function averagePoints(vertices, tl, tr, bl, br, elevation) {
   let sum = vertices[tl].z + vertices[tr].z + vertices[bl].z + vertices[br].z;
-  return (sum / 4) + rr() * damping;
+  return (sum / 4) + rr() * elevation;
 }
 
 var diamond; // Just to avoid a JSHint warning
 
-function square(vertices, tx, ty, bx, by, damping) {
+/**
+ * The recursive square pass of the diamond-square algorithm.
+ */
+function square(vertices, tx, ty, bx, by, elevation) {
 
   let half = (bx - tx) / 2;
 
@@ -41,35 +48,36 @@ function square(vertices, tx, ty, bx, by, damping) {
   let bl = coordsToIndex(tx, by);
   let br = coordsToIndex(bx, by);
 
-  // console.log('middle',tl,tr,bl,br);
-  // console.log('avg', averagePoints(vertices, tl, tr, bl, br));
-  vertices[middle].z = averagePoints(vertices, tl, tr, bl, br, damping);
+  vertices[middle].z = averagePoints(vertices, tl, tr, bl, br, elevation);
 
-  damping *= TERRAIN_DAMPING_FACTOR;
+  elevation *= TERRAIN_DAMPING_FACTOR;
 
   // top diamond
-  diamond(vertices, tx, ty, middleX, ty - half, damping);
+  diamond(vertices, tx, ty, middleX, ty - half, elevation);
   // right diamond
-  diamond(vertices, middleX, middleY, bx, ty, damping);
+  diamond(vertices, middleX, middleY, bx, ty, elevation);
   // bottom diamond
-  diamond(vertices, tx, by, middleX, middleY, damping);
+  diamond(vertices, tx, by, middleX, middleY, elevation);
   // left diamond
-  diamond(vertices, tx - half, middleY, tx, ty, damping);
+  diamond(vertices, tx - half, middleY, tx, ty, elevation);
 
   // Squares again
-  damping *= TERRAIN_DAMPING_FACTOR;
+  elevation *= TERRAIN_DAMPING_FACTOR;
 
   // top left square
-  square(vertices, tx, ty, tx + half, ty + half, damping);
+  square(vertices, tx, ty, tx + half, ty + half, elevation);
   // top right square
-  square(vertices, tx + half, ty, bx, ty + half, damping);
+  square(vertices, tx + half, ty, bx, ty + half, elevation);
   // bottom left square
-  square(vertices, tx, ty + half, tx + half, by, damping);
+  square(vertices, tx, ty + half, tx + half, by, elevation);
   // bottom right square
-  square(vertices, tx + half, ty + half, bx, by, damping);
+  square(vertices, tx + half, ty + half, bx, by, elevation);
 }
 
-function diamond(vertices, lx, ly, tx, ty, damping) {
+/**
+ * The diamon pass of the diamon-square algorithm
+ */
+function diamond(vertices, lx, ly, tx, ty, elevation) {
   let dist = tx - lx;
 
   let middleX = tx;
@@ -81,12 +89,15 @@ function diamond(vertices, lx, ly, tx, ty, damping) {
   let r = coordsToIndex(middleX + dist, ly);
   let b = coordsToIndex(middleX, middleY + dist);
 
-  vertices[middle].z = averagePoints(vertices, l, t, r, b, damping);
+  vertices[middle].z = averagePoints(vertices, l, t, r, b, elevation);
 
-  damping += TERRAIN_DAMPING_FACTOR;
+  elevation += TERRAIN_DAMPING_FACTOR;
 }
 
-function diamondSquare(vertices) {
+/**
+ * Assign z values to the vertex list using the diamond-square algorithm
+ */
+function generateTerrain(vertices) {
   // Initialize corner values
 
   square(vertices,
@@ -95,6 +106,13 @@ function diamondSquare(vertices) {
          TERRAIN_INITIAL_DAMPING);
 
 }
+
+/*************************************
+ *
+ * Main game functions
+ *
+ *
+ *************************************/
 
 (function() {
 
@@ -110,7 +128,7 @@ function diamondSquare(vertices) {
         this.geometry = new THREE.PlaneGeometry(data.size, data.size,
                                                 TERRAIN_SEGMENTS, TERRAIN_SEGMENTS);
 
-        diamondSquare(this.geometry.vertices);
+        generateTerrain(this.geometry.vertices);
 
         this.geometry.rotateX(-Math.PI/2);
 
@@ -118,10 +136,6 @@ function diamondSquare(vertices) {
           color: 0x7d4745,
           shading: THREE.FlatShading,
         });
-        //this.material = new THREE.MeshPhongMaterial({
-        //  color: 0x00ff88,
-          //wireframe: true
-        //});
 
         this.mesh = new THREE.Mesh(this.geometry, this.material);
         this.mesh.scale.set(TERRAIN_SCALE_FACTOR,1,TERRAIN_SCALE_FACTOR);
@@ -135,16 +149,11 @@ function diamondSquare(vertices) {
 
     let scene = document.getElementById('scene');
 
-
     let terrain = document.createElement('a-entity');
     terrain.setAttribute('terrain', `size: ${TERRAIN_SIZE}`);
     terrain.setAttribute('position', {x: 0, y: -10,z: 0});
 
     scene.appendChild(terrain);
-
-
-
-
   };
 
 })();
